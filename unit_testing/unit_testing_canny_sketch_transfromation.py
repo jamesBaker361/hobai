2e4b47e338a180e510cc0cfdd218643256bb5c89
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-06-29 19:54:26 -0400

    more images

--- unit_testing/unit_testing_canny_sketch_transfromation.py
+++ unit_testing/unit_testing_canny_sketch_transfromation.py
@@ -9,12 +9,14 @@ import unittest
 
 class CannySketchTransformationTestCase(unittest.TestCase):
     def setUp(self):
-        self.pipeline=Pipeline([CannySketchTransformation()], [64])
+        self.patch_sizes=[32,64,256]
+        self.pipeline=Pipeline([CannySketchTransformation()], self.patch_sizes)
         self.img=Image.open('cat.png')
 
     def test_call(self):
-        transformed_img=self.pipeline(self.img)[0]
-        transformed_img.save(UNIT_TESTING_OUTPUT_DIR+'/test_canny_sketch_call.png')
+        for x,p_size in enumerate(self.patch_sizes):
+            transformed_img=self.pipeline(self.img)[x]
+            transformed_img.save(UNIT_TESTING_OUTPUT_DIR+'/test_canny_sketch_call_{}.png'.format(p_size))
 
 if __name__=='__main__':
     unittest.main() # run all tests
